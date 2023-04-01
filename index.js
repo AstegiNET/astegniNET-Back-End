00390e5d56f471010e88ea1b142d32ce1c54d62d
kaleabbyh <kaleabbyh@gmail.com>
2023-04-01 17:35:22 +0300

    video call route and controller added

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ const port = process.env.PORT || 5000;
 app.use("/api/tutees", require("./routes/tuteeRoutes"));
 app.use("/api/tutors", require("./routes/tutorRoutes"));
 app.use("/api/courses", require("./routes/courseRoutes"));
+app.use("/videocall", require("./routes/videoRoute"));
 
 app.use(errorHandler);
 // Start the server
