15439bd794543c9a740fc6802b8489c4967da59d
fikireab-m <fikireab.meku@gmail.com>
2023-04-04 13:37:52 +0300

    try

--- config/db.js
+++ config/db.js
@@ -3,7 +3,10 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     const uri = process.env.MGDB;
-    await mongoose.connect(uri);
+    await mongoose.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
     console.log("successfully connected to mongodb");
   } catch (err) {
     console.log(err);
@@ -11,3 +14,4 @@ const connectDB = async () => {
 };
 
 module.exports = connectDB;
+
