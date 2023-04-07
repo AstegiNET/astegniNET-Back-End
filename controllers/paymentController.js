0760eb00acbf0176b118f0299c11146c2dfc67fd
kaleabbyh <kaleabbyh@gmail.com>
2023-04-07 12:42:58 +0300

    payment controller is added

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -7,19 +7,24 @@ const chapa = new Chapa({
 
 const secretKey = process.env.CHAPA_SECRET_KEY;
 const base_url = "https://api.chapa.co/v1/transaction/initialize";
+const verify_url = "https://api.chapa.co/v1/transaction/verify/";
+
+//config
+
+const config = {
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+    Authorization: `Bearer ${secretKey}`,
+  },
+};
 
 //initialize payment
 const InitializePayent = async (req, res) => {
   const inputData = req.body;
 
   await axios
-    .post(base_url, inputData, {
-      headers: {
-        "Content-Type": "application/json",
-        Accept: "application/json",
-        Authorization: `Bearer ${secretKey}`,
-      },
-    })
+    .post(base_url, inputData, config)
     .then((response) => {
       console.log(response.data.data);
       res.send(JSON.stringify(response.data.data));
@@ -30,4 +35,20 @@ const InitializePayent = async (req, res) => {
     });
 };
 
-module.exports = { InitializePayent };
+// verification endpoint
+const verify = async (req, res) => {
+  //verify the transaction
+  await axios
+    .get(verify_url + req.params.id, config)
+    .then((response) => {
+      console.log(response.data);
+    })
+    .catch((err) => console.log("Payment can't be verfied", err));
+};
+
+// success callback
+const success = async (req, res) => {
+  res.render("success");
+};
+
+module.exports = { InitializePayent, verify, success };
--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -1,7 +1,13 @@
 const router = require("express").Router();
-const { InitializePayent } = require("../controllers/paymentController");
+const {
+  InitializePayent,
+  verify,
+  success,
+} = require("../controllers/paymentController");
 const { protectTutee } = require("../middleware/authMiddleware");
 
 router.post("/pay", protectTutee, InitializePayent);
+router.get("/pay/verify/:id", protectTutee, verify);
+router.get("/pay/success", protectTutee, success);
 
 module.exports = router;
